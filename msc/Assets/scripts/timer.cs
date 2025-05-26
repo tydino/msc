71ad6ddd99fc07e0cd1c250429216cdfee1e9bc8
tydino <tydino909@gmail.com>
2025-05-25 22:43:19 -0400

    song skip system
    it needs to be filled in manually for every bar of the song, but once done, it will skip over parts where the creature is asleep.
    
    it is broken currently regarding the bass as it is set up in neglected nostalgia to skip if it is in, but due to me not including the base into the overall for the creatures it does break in that regard. (I plan on doing something like the lost landscapes did for evergreen marsh where it actually comes from something, and in this case it also allows me to make a specific creature or thing that will only play the bass. I will work on that thing tommorrow, though i do know for at least the dreamicks it will be a little speaker and its off state will have it not vibrate or make noise!) while on that i will also rework the animation system a bit so that like when the creature goes to sleep, when it goes into idle it will turn off the audio source.

--- msc/Assets/scripts/timer.cs
+++ msc/Assets/scripts/timer.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class timer : MonoBehaviour
 {
+    public List<t_creaturesNeeded> creaturesNeededForEachTime;
+    [Header("limit is 1 less then song's true end")]
     public int limit;
     public Animator a;
     public static int Timer;
@@ -23,5 +25,37 @@ public class timer : MonoBehaviour
     public void Tick()
     {
         Timer++;
+        /*if (Timer! > 3)
+        {*/
+            bool contains = false;
+            while (contains == false)
+            {
+                foreach (int ID in creaturesNeededForEachTime[Timer].creatureIdNeeded)
+                {
+                    foreach (sd_CreatureHandler sdh in creatureHandler.current.creatureInformation)
+                    {
+                        if (sdh.CreatureID == ID && sdh.asleep == false)
+                        {
+                            contains = true;
+                        }
+                    }
+                }
+                if (contains == false)
+                {
+                    Timer++;
+                }
+                if (limit < Timer)
+                {
+                    Timer = 0;
+                    break;
+                }
+            }
+        //}
     }
+}
+
+[System.Serializable]
+public class t_creaturesNeeded
+{
+    public List<int> creatureIdNeeded;
 }
